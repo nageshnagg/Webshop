5461b13cbe13ba7e3fc5bdc7a08fc059b5b5505c
nageshnagg <rockey24731@gmail.com>
2021-10-26 11:58:21 +0530

    i added new package name is pages

---
+++ src/main/java/Ecomerce/Pages.java
@@ -0,0 +1,32 @@
+package Ecomerce;
+
+public class Pages {
+public void BillingAddress() {
+	System.out.println("BillingAddress");
+}
+public void Checkout() {
+	System.out.println(" Checkout");
+}
+ public void ClearShopping() {
+	System.out.println("ClearShopping");
+}
+public void cofimrorder() {
+	System.out.println("cofimrorder");
+}
+public void GoTOCart() {
+	System.out.println(" GoTOCart");
+}
+public void login() {
+	System.out.println("login");
+}
+public void logout() {
+	System.out.println("logout");
+}
+public void paymentinfo() {
+	System.out.println(" paymentinfo");
+}
+public void paymentMethod() {
+	System.out.println(" paymentMethod");
+}
+
+}
